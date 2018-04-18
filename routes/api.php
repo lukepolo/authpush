4453157a71af8347ff3145b81d9c3fe747fb0f53
Luke Policinski <Luke@LukePOLO.com>
2018-04-17 22:29:56 -0400

    remo

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,6 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::apiResource('applications', 'ApplicationsController');
 
     Route::get('accounts/{account}/otp/approve', function ($account) {
-        ;
-
         broadcast(new \App\Events\Approved(
             \App\Application::findOrFail($account),
             \Auth::user()
@@ -18,4 +16,5 @@ Route::group(['middleware' => 'auth:api'], function () {
 
 header("Access-Control-Allow-Origin: *");
 Route::post('/otp/request', function () {
+    // we send request to users 2nd auth application that we have to build yet ><
 });
