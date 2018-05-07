3a6ea757ca6ec8b1b014ead026b9b46766ee0943
Luke Policinski <luke.policinski@egov.com>
2018-05-07 12:51:04 -0400

    removal of unused controller, we are storing the OTP on their devices, we only need to use when we send it along

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,6 @@
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::apiResource('accounts', 'AccountsController')->except('update');
-    Route::get('accounts/{account}/otp', 'AccountOneTimePasswordController');
-
     Route::apiResource('applications', 'ApplicationsController');
 });
 
