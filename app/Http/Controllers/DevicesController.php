a53a06a55a36c7f04646268801b26a765cd88b0b
Luke Policinski <luke.policinski@egov.com>
2018-05-07 12:49:06 -0400

    more Renaming

--- app/Http/Controllers/DevicesController.php
+++ app/Http/Controllers/DevicesController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TokenController extends Controller
+class DevicesController extends Controller
 {
     public function store(Request $request)
     {
         $credentials = $request->only('email', 'password');
+
+        // TODO - create the device
 
         if (\Auth::attempt($credentials)) {
             return \Auth::user()->createToken('authpush-mobile');
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,6 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::apiResource('applications', 'ApplicationsController');
 });
 
-Route::post('token', 'TokenController@store');
-Route::post('/request/approval', 'AuthRequestController@store');
-Route::post('/request/{requestHash}/approve', 'ApproveAuthRequestController@store');
+Route::post('devices', 'DevicesController@store');
+Route::post('request/approval', 'AuthRequestController@store');
+Route::post('request/{requestHash}/approve', 'ApproveAuthRequestController@store');
