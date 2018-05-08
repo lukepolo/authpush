61b85e31a4e23c6ec2f5f4f5bf26f8ef96c36aac
Luke Policinski <luke@lukepolo.com>
2018-05-08 15:40:50 -0400

    nullabe

--- database/migrations/2018_04_17_150452_create_devices_table.php
+++ database/migrations/2018_04_17_150452_create_devices_table.php
@@ -18,7 +18,7 @@ class CreateDevicesTable extends Migration
             $table->unsignedInteger('user_id');
             $table->string('name');
             $table->string('type');
-            $table->longText('notification_token');
+            $table->longText('notification_token')->nullabe();
             $table->timestamps();
         });
     }
