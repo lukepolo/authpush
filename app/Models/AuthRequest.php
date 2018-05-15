26fa3013baab0b985ba9bfbe1e41d0ef083a0f0f
Luke Policinski <luke@lukepolo.com>
2018-05-14 21:37:18 -0400

    adding relations

--- app/Models/AuthRequest.php
+++ app/Models/AuthRequest.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class AuthRequest extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public function account()
+    {
+        return $this->belongsTo(Account::class);
+    }
 }
